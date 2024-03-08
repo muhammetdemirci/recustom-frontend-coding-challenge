439a374797643ff85889e1d42873f87f60b0604a
Muhammet Demirci <mddemircii5@gmail.com>
2024-03-08 23:41:54 +0300

    add colors

---
+++ src/theme/colors.tsx
@@ -0,0 +1,18 @@
+export const COLORS = {
+    success300: '#66E7C6',
+    success600: '#00AC80',
+
+    danger300: '#FFA2A2',
+    danger500: '#FF6464',
+
+    gray400: '#9CA3AF',
+    gray900: '#111928',
+
+    white: '#FFFFFF',
+
+    black: '#000000',
+
+    sun500: '#FFFF00',
+
+    midnight500: '#112127',
+}
